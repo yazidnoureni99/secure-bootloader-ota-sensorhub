ca7432606b5ae980c06298efae2f2aa3175094bb
Yazid NOURENI <yezidnoureni@exemple.com>
2025-12-23 21:57:49 +0100

    Ajout de fichier .gitgnore pour le workspace

---
+++ Bootloader/Bootloader/Core/Inc/flash_layout.h
@@ -0,0 +1,14 @@
+/*
+ * flash_layout.h
+ *
+ *  Created on: 23 déc. 2025
+ *      Author: Yazid NOURENI
+ */
+
+#ifndef INC_FLASH_LAYOUT_H_
+#define INC_FLASH_LAYOUT_H_
+
+#define BL_START_ADR 0X08000000 // 32KB
+#define APP1_START_ADR 0X08008000 //224 KB
+
+#endif /* INC_FLASH_LAYOUT_H_ */
